c7d05e74672e9ff81163e19330e71698ece557ff
mateuszkl <mateusz.klepacki1@gmail.com>
2019-11-17 15:13:51 +0100

    Mono part 2 done, Part 3 in progress

--- src/main/java/io/pivotal/literx/Part03StepVerifier.java
+++ src/main/java/io/pivotal/literx/Part03StepVerifier.java
@@ -20,6 +20,7 @@ import java.util.function.Supplier;
 
 import io.pivotal.literx.domain.User;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 /**
  * Learn how to use StepVerifier to test Mono, Flux or any other kind of Reactive Streams Publisher.
@@ -33,29 +34,40 @@ public class Part03StepVerifier {
 
 	// TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then completes successfully.
 	void expectFooBarComplete(Flux<String> flux) {
-		fail();
+		StepVerifier.create(flux)
+				.expectNext("foo")
+				.expectNext("bar")
+				.verifyComplete();
 	}
 
 //========================================================================================
 
 	// TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then a RuntimeException error.
 	void expectFooBarError(Flux<String> flux) {
-		fail();
+		StepVerifier.create(flux)
+				.expectNext("foo", "bar")
+				.verifyError(RuntimeException.class);
 	}
 
 //========================================================================================
 
-	// TODO Use StepVerifier to check that the flux parameter emits a User with "swhite"username
+	// TODO Use StepVerifier to check that the flux parameter emits a User with "swhite" username
 	// and another one with "jpinkman" then completes successfully.
 	void expectSkylerJesseComplete(Flux<User> flux) {
-		fail();
+        StepVerifier.create(flux)
+                .expectNextMatches(user -> "swhite".equalsIgnoreCase(user.getUsername()))
+                .expectNextMatches(user -> "jpinkman".equalsIgnoreCase(user.getUsername()))
+                .verifyComplete();
 	}
 
 //========================================================================================
 
 	// TODO Expect 10 elements then complete and notice how long the test takes.
 	void expect10Elements(Flux<Long> flux) {
-		fail();
+		StepVerifier.create(flux)
+				.expectNextCount(10)
+				.verifyComplete();
+
 	}
 
 //========================================================================================
